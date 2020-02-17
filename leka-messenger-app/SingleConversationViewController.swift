4d22319cd1be716c3c97d7c191042fea0066b7ad
Robbie Gay <robbiecgay@gmail.com>
2020-02-17 15:50:59 -0500

    Created todo list for MVP launch

--- leka-messenger-app/SingleConversationViewController.swift
+++ leka-messenger-app/SingleConversationViewController.swift
@@ -16,3 +16,15 @@ class SingleConversationViewController: UITableViewController {
         view.backgroundColor = .purple
     }
 }
+
+/*
+ - Make nav bar title the persons name
+ - populate tableview with messages in the feed
+ - send view of screen to bottom
+ - create a way to type a message (screen should move up on keyborad)
+ - create a way to send that message to the database (might need to create a model file)
+ - create a way to update the messages
+ - Create a "unread message" icon (and figure out how to make it "read"
+ - Add this message feature to the new conversation page
+ - soft message in AllConversations by most recent (general messages would need to be softed in this manner too)
+ */
